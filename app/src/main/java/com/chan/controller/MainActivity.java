33ffea668d1f5e468443b68472a5195edc666ac1
chenzhengyang@njrobot.com <chenzhengyang@njrobot.com>
2018-11-19 18:18:25 +0800

    d

--- app/src/main/java/com/chan/controller/MainActivity.java
+++ app/src/main/java/com/chan/controller/MainActivity.java
@@ -1,6 +1,7 @@
 package com.chan.controller;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,9 @@ import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,6 +21,8 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean flag = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +30,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
 //        查询按钮
-        Button searchbtn = (Button) findViewById(R.id.search_bluetooth);
-//        searchbtn.setOnClickListener();
+        Button searchbtn = findViewById(R.id.search_bluetooth);
+        TextView bluetoothicon = findViewById(R.id.bluetooth_icon);
+        final AnimationDrawable anim = (AnimationDrawable) bluetoothicon.getBackground();
+        searchbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = !flag;
+                if(flag) {
+                    anim.start();
+                    Toast.makeText(MainActivity.this,"true",Toast.LENGTH_LONG).show();
+                }else{
+                    anim.stop();
+                    anim.selectDrawable(0);
+                    Toast.makeText(MainActivity.this,"false",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
 
 
@@ -50,12 +71,15 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Map<String,Object> map = (Map<String,Object>)adapterView.getItemAtPosition(i);
                 Toast.makeText(MainActivity.this,map.get("id").toString(),Toast.LENGTH_SHORT).show();
+                anim.stop();
                 Intent intent = new Intent(MainActivity.this,MonitorActivity.class);
                 startActivity(intent);
             }
         });
 
     }
+
+
 
 
 }
